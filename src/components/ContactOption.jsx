4868f54b137fa3c92ffbef55dd388085f4487b65
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-18 22:52:36 +0600

    change bg

--- src/components/ContactOption.jsx
+++ src/components/ContactOption.jsx
@@ -7,14 +7,14 @@ const StickyContact = () => {
       {/* Phone Icon */}
       <Link
         href="tel:+8801302-216999"
-        className="flex items-center justify-center bg-blue-500 text-white p-3 rounded-full shadow-lg transform transition duration-300 hover:scale-110 hover:bg-blue-600"
+        className="flex items-center justify-center text-white p-3 rounded-full shadow-lg transform transition duration-300 hover:scale-110 hover:bg-blue-600"
       >
         <FaPhoneAlt className="w-6 h-6" />
       </Link>
       {/* Gmail Icon */}
       <Link
         href="mailto:shasan2200@gmail.com"
-        className="flex items-center justify-center bg-red-500 text-white p-3 rounded-full shadow-lg transform transition duration-300 hover:scale-110 hover:bg-red-600"
+        className="flex items-center justify-center text-white p-3 rounded-full shadow-lg transform transition duration-300 hover:scale-110 hover:bg-red-600"
       >
         <FaEnvelope className="w-6 h-6" />
       </Link>
@@ -23,7 +23,7 @@ const StickyContact = () => {
         href="https://www.linkedin.com/in/sharifuzzaman-hasan-0b561521a/"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center justify-center bg-blue-700 text-white p-3 rounded-full shadow-lg transform transition duration-300 hover:scale-110 hover:bg-blue-800"
+        className="flex items-center justify-center  text-white p-3 rounded-full shadow-lg transform transition duration-300 hover:scale-110 hover:bg-blue-800"
       >
         <FaLinkedin className="w-6 h-6" />
       </Link>
