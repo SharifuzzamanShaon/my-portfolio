d62bbf21d03cac7f16a6a09d3ffaf6c57b1d18dd
sharifuzzamanShaon <shasan2200@gmail.com>
2025-02-19 21:45:29 +0600

    add data to projects

--- src/ProjectData/Projects.js
+++ src/ProjectData/Projects.js
@@ -10,7 +10,7 @@ const projects = [
     },
     {
       id: 2,
-      name: "University management System | UMS-app | --API endpoints",
+      name: "University management System | API's",
       image: "/projectImg/api-ums.png",
       description: "Backend to Integrate the online attendance system using Node js and Mongo DB Database, The main goal of this project is to focus on code/function reusability and reduce the complexity of development. Token-based Auth Using JWT || cron job schedule  || code reusability || Error handling, and complete folder structure",
       liveLink: "",
@@ -21,7 +21,7 @@ const projects = [
       id: 3,
       name: "Find Recipes",
       image: "/projectImg/recipe.png",
-      description: "This Recipe Management App enables users to explore and manage recipes effortlessly. It features secure user authentication with session management via localStorage, client-side pagination for faster and smoother recipe browsing, and a cart system to add recipes from any page. Cart data is stored locally for guests and linked to accounts for logged-in users. Additional enhancements include React Hot Toast for instant notifications, Material UI for a modern design",
+      description: "This Recipe Management App enables users to explore and manage recipes effortlessly. It features secure user authentication with session management via localStorage, client-side pagination . Cart data is stored locally for guests and linked to accounts for logged-in users. ",
       liveLink: "https://recipe-app-coral-one.vercel.app/",
       gitRepo: "https://github.com/SharifuzzamanShaon/recipe-app-frontend",
       technologies: ["Next JS", "MVP features", "axios", "Tailwind CSS"],
