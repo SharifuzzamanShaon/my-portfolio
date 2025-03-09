b0021ee39acd64eb6f7f4d1e9da48ded75912b95
sharifuzzamanshaon <shasan2200@gmail.com>
2025-03-09 19:26:17 +0600

    update HomePage component to implement smooth scrolling and refactor project and contact sections

--- src/pages/Home-page.jsx
+++ src/pages/Home-page.jsx
@@ -10,42 +10,52 @@ import HeadingSection from "../components/HeadingSection";
 import StickyContact from "../components/ContactOption";
 import ContactPage from "./ContactPage";
 import DescriptionSection from "../components/DescriptionSection";
+import Header from "../components/Headers";
+import { useRef } from "react";
 const HomePage = ({ darkMode }) => {
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+  const handleScroll = (ref) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <>
+      <Header contactRef={contactRef} projectsRef={projectsRef} />
       <div className="min-h-screen bg-gradient-to-r from-[#2d3363] to-[#bbbbbb] flex items-center justify-center text-white px-6 py-12">
         <div className="text-center max-w-5xl space-y-10">
           <HeadingSection />
           <StickyContact />
           {/* Description Section */}
-          <DescriptionSection />
           {/* CTA Button */}
+          <DescriptionSection />
           <div className="mt-6">
-            <Link to="/projects">
-              <span className=" bg-gradient-to-r from-[#5c5a52] to-[#3a3a3a]  font-semibold py-2 px-6 rounded-full text-md shadow-xl hover:shadow-2xl text-white transform transition duration-300 hover:scale-105">
+            <button onClick={() => handleScroll(projectsRef)}>
+              <span className=" bg-slate-500 p-1  font-semibold py-2 px-6 rounded-full text-md shadow-xl hover:shadow-2xl text-white transform transition duration-300 hover:scale-105">
                 View My Projects
               </span>
-            </Link>
+            </button>
           </div>
-          {/* Display My Skill */}
-          {/* Porjects */}
         </div>
       </div>
       <MySkills />
-      <div className="bg-gradient-to-r from-[#2d3363] to-[#bbbbbb] sm:grid-cols-2">
-        <div className="text-center py-4">
-          <h2 className="text-2xl sm:text-3xl font-semibold tracking-tight text-white mb-2 relative inline-block">
-            <span className="relative z-5">Projects</span>
-            <span className="absolute inset-x-0 -bottom-1 h-1 bg-gradient-to-r from-yellow-400 to-orange-500 rounded-lg"></span>
-          </h2>
+      <section ref={projectsRef}>
+        <div className="bg-gradient-to-r from-[#2d3363] to-[#bbbbbb] sm:grid-cols-2">
+          <div className="text-center py-4">
+            <h2 className="text-sm sm:text-3xl tracking-tight text-white mb-2 relative inline-block">
+              <span className="bg-slate-500 p-1 py-2 px-6 rounded-full  relative z-5">Projects</span>
+              {/* <span className="absolute inset-x-0 -bottom-1 h-1 bg-gradient-to-r from-yellow-400 to-orange-500 rounded-lg"></span> */}
+            </h2>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-4 max-w-6xl mx-auto px-4">
+            {projects?.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 py-4 max-w-6xl mx-auto px-4">
-          {projects?.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
-        </div>
-      </div>
-      <ContactPage />
+      </section>
+      <section ref={contactRef}>
+        <ContactPage />
+      </section>
     </>
   );
 };
