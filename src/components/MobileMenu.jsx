cac01605267d5f2dbe188c58a384f9114d9e23c0
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-29 18:52:17 +0600

    refactor MobileMenu component: simplify structure and remove unused imports

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,49 +1,36 @@
 import React from "react";
 import { BsDownload } from "react-icons/bs";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const MobileMenu = () => {
+const MobileMenu = ({projectsRef,contactRef}) => {
   const pdfUrl = import.meta.env.VITE_CV_DRIVE_URL;
-  const location = useLocation();
   const handleOpenPdf = () => {
     window.open(pdfUrl, "_blank"); // Opens the PDF in a new tab
   };
   return (
-    <nav className="flex space-x-4">
-      <button onClick={handleOpenPdf} className="flex items-center space-x-2">
-        <a
-          className={`${
-            location.pathname === "/resume"
-              ? "text-yellow-400"
-              : "text-blue-700 dark:text-blue-700"
-          } hover:text-yellow-500 transition text-sm font-semibold tracking-wide flex items-center space-x-2`}
-        >
-          <span>RESUME</span>
-          <BsDownload className="text-sm" />
-        </a>
-      </button>
-      <Link to="/projects">
-        <span
-          className={`${
-            location.pathname === "/projects"
-              ? "text-yellow-400"
-              : "text-gray-300 dark:text-gray-400"
-          } hover:text-yellow-500 transition text-sm font-semibold tracking-wide`}
+    <nav>
+      <div className="flex space-x-4">
+        <button onClick={handleOpenPdf} className="flex items-center space-x-2">
+          <a
+            className={`text-yellow-400 hover:text-yellow-500 transition text-sm font-semibold tracking-wide flex items-center space-x-2`}
+          >
+            <span>RESUME</span>
+            <BsDownload className="text-sm" />
+          </a>
+        </button>
+        <button
+          onClick={() => projectsRef.current?.scrollIntoView({ behavior: "smooth" })}
+          className={`text-gray-300 dark:text-gray-400 hover:text-yellow-500 transition text-sm font-semibold tracking-wide`}
         >
           PROJECTS
-        </span>
-      </Link>
-      <Link to="/contact">
-        <span
-          className={`${
-            location.pathname === "/contact"
-              ? "text-yellow-400"
-              : "text-gray-300 dark:text-gray-400"
-          } hover:text-yellow-500 transition text-sm font-semibold tracking-wide`}
+        </button>
+        <button
+          onClick={() => contactRef.current?.scrollIntoView({ behavior: "smooth" })}
+          className={`text-gray-300 dark:text-gray-400 hover:text-yellow-500 transition text-sm font-semibold tracking-wide`}
         >
           CONTACT
-        </span>
-      </Link>
+        </button>
+      </div>
     </nav>
   );
 };
