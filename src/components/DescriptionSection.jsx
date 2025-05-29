d2497df10bdee7c048b3521509dcf88c8525b829
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-29 18:32:14 +0600

    refactor DescriptionSection component: update font styles, remove unused imports, and pass scrollToProjects as a prop

--- src/components/DescriptionSection.jsx
+++ src/components/DescriptionSection.jsx
@@ -1,26 +1,21 @@
-import React, { useRef } from "react";
+import React from "react";
 import { BsDownload } from "react-icons/bs";
-import { useLocation } from "react-router";
 
-const DescriptionSection = () => {
+const DescriptionSection = ({ scrollToProjects }) => {
   const pdfUrl = import.meta.env.VITE_CV_DRIVE_URL;
   const handleOpenPdf = () => {
     window.open(pdfUrl, "_blank"); // Opens the PDF in a new tab
   };
-  const projectsRef = useRef(null);
-  const handleScroll = (ref) => {
-    ref.current?.scrollIntoView({ behavior: "smooth" });
-  };
   return (
     <div className="max-w-7xl mx-auto px-4 py-8">
       <h2 className="text-lg sm:text-2xl font-semibold text-white mb-4 relative inline-block">
-        <span className="relative z-5  font-thin  bg-slate-500 p-1 rounded-lg px-4 font-mono">
-        {`<About_Me/>`}  
+        <span className="relative z-5  font-light  bg-slate-500 p-1 rounded-lg px-4 font-poppins">
+        {`About Me`}  
         </span>
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 text-gray-200 text-start mt-4">
         <div>
-          <p className="text-sm sm:text-md leading-relaxed mb-4 font-mono">
+          <p className="text-sm sm:text-md leading-relaxed mb-4 font-poppins">
             Hi, I'm{" "}
             <span className="font-sm text-yellow-400">Sharifuzzaman Hasan</span>
             , a passionate Full-Stack Developer and final-year Computer Science
@@ -28,7 +23,7 @@ const DescriptionSection = () => {
             <span className="font-bold">MERN stack</span> (MongoDB, Express.js,
             React.js, Node.js) and JavaScript.
           </p>
-          <p className="text-sm sm:text-md leading-relaxed mb-4 font-mono">
+          <p className="text-sm sm:text-md leading-relaxed mb-4 font-poppins">
             I specialize in building innovative and scalable web solutions, with
             hands-on experience in{" "}
             <span className="font-bold">frontend development</span> (React.js,
@@ -37,7 +32,7 @@ const DescriptionSection = () => {
             <span className="font-bold">database management</span> (MongoDB,
             MySQL).
           </p>
-          <p className="text-sm sm:text-md leading-relaxed mb-4 font-mono">
+          <p className="text-sm sm:text-md leading-relaxed mb-4 font-poppins">
             Driven by problem-solving and collaboration, I strive to create
             seamless digital experiences.{" "}
             <span className="font-bold text-yellow-400">
@@ -52,29 +47,29 @@ const DescriptionSection = () => {
               className="flex items-center gap-2 border border-gray-800 px-4 py-2 rounded-lg text-white font-semibold 
              transition-all duration-300 hover:bg-gray-800 hover:text-white hover:scale-105 active:scale-95 shadow-md"
             >
-              <span className="animate-pulse font-mono">Find My Resume</span>
+              <span className="animate-pulse font-poppins">Find My Resume</span>
               <BsDownload className="text-xl" />
             </button>
 
             {/* View My Projects Button */}
             <button
-              onClick={() => handleScroll(projectsRef)}
+              onClick={scrollToProjects}
               className="flex items-center gap-2 border border-gray-800 px-4 py-2 rounded-lg text-white font-semibold 
              transition-all duration-300 hover:bg-gray-800 hover:text-white hover:scale-105 active:scale-95 shadow-md"
             >
-              <span className="animate-pulse font-mono">View My Projects</span>
+              <span className="animate-pulse font-poppins">View My Projects</span>
             </button>
           </div>
         </div>
 
         {/* Education Background Section */}
         <div>
-          <h3 className="text-xl sm:text-md font-sm text-yellow-400 mb-4 font-light font-mono">
+          <h3 className="text-xl sm:text-md font-sm text-yellow-400 mb-4 font-light font-poppins">
             Education
           </h3>
           <div className="space-y-4">
-            <p className="text-sm sm:text-lg leading-relaxed text-left font-mono">
-              <span className="font-bold bg-gray-800 p-1 font-mono">
+            <p className="text-sm sm:text-lg leading-relaxed text-left font-poppins">
+              <span className="font-bold bg-gray-800 p-1 font-poppins">
                 BSc in Computer Science & Engineering
               </span>
               <br />
@@ -84,7 +79,7 @@ const DescriptionSection = () => {
                 Expected Graduation Date: November 2025
               </span>
             </p>
-            <p className="text-sm sm:text-lg leading-relaxed text-left font-mono">
+            <p className="text-sm sm:text-lg leading-relaxed text-left font-poppins">
               <span className="font-bold bg-gray-800 p-1">
                 Diploma in Computer Technology
               </span>
