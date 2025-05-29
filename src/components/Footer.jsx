d970ba81896020d2184844ea524eecb9f541c663
sharifuzzamanshaon <shasan2200@gmail.com>
2025-05-29 18:32:28 +0600

    add Footer component with copyright information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="flex justify-center items-center bg-gradient-to-r from-[#2d3363] to-[#bbbbbb] dark:bg-gray-900 pb-3">
+                <h2 className="text-lg font-light font-poppins text-gray-800">Sharifuzzaman Hasan</h2>
+                <p className="text-sm font-poppins text-gray-800 ml-2">Copyright © 2025</p>
+            </div>
+        </footer>
+    );
+};
+export default Footer;
